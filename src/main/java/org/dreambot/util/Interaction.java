e7a7876803fc517ef3cca6dd80ec28f717b73cac
Amoz3 <camalCase@protonmail.com>
2022-07-12 00:34:36 +1000

    added delayed inventory interactions

--- src/main/java/org/dreambot/util/Interaction.java
+++ src/main/java/org/dreambot/util/Interaction.java
@@ -1,6 +1,7 @@
 package org.dreambot.util;
 
 import org.dreambot.api.methods.MethodProvider;
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.wrappers.interactive.Entity;
 
 public class Interaction {
@@ -12,5 +13,16 @@ public class Interaction {
     public static boolean delayEntityInteract(Entity entity, long sleepDelay) {
         MethodProvider.sleep((int) sleepDelay);
         return entity.interact();
+    }
+
+    // inventory interact is a little safer because it checks you are on the right tab
+    public static boolean delayInventoryInteract(int id, String action, long sleepDelay) {
+        MethodProvider.sleep((int) sleepDelay);
+        return Inventory.interact(id, action);
+    }
+
+    public static boolean delayInventoryInteract(String name, String action, long sleepDelay) {
+        MethodProvider.sleep((int) sleepDelay);
+        return Inventory.interact(name, action);
     }
 }
