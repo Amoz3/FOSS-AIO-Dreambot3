75f13b12dea2ca67ea8f8ae5304df0dc86df8b0c
Amoz3 <camalCase@protonmail.com>
2022-07-12 00:05:30 +1000

    more with the f2p mining

--- src/main/java/org/dreambot/task/mining/f2pmining/F2PMiningLeaf.java
+++ src/main/java/org/dreambot/task/mining/f2pmining/F2PMiningLeaf.java
@@ -1,8 +1,16 @@
 package org.dreambot.task.mining.f2pmining;
 
 import org.dreambot.Main;
+import org.dreambot.api.methods.MethodProvider;
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.container.impl.equipment.Equipment;
+import org.dreambot.api.methods.filter.Filter;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
 import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.wrappers.items.Item;
+import org.dreambot.data.mining.Pickaxe;
 import org.dreambot.framework.Leaf;
 import org.dreambot.util.Timing;
 /*
@@ -11,6 +19,12 @@ import org.dreambot.util.Timing;
     15 - 99 iron ore @
  */
 public class F2PMiningLeaf extends Leaf<Main> {
+    Pickaxe bestOwnedPickaxe;
+    int cachedLvl = 0; // this is to skip the for loop until you have leveled up
+    // used to detect if you have items you shouldnt when mining so you can go put them away
+    Filter<Item> miningFilter = x -> !x.getName().toLowerCase().contains("ore")
+            || !x.getName().toLowerCase().contains("uncut")
+            || !x.getName().toLowerCase().contains("pickaxe");
     @Override
     public boolean isValid() {
         return true;
@@ -31,6 +45,38 @@ public class F2PMiningLeaf extends Leaf<Main> {
                 return 100;
             }
         }
+        // now we have a bank cache, we need to find our best owned pickaxe and make sure we have that equipped / in inv
+        for (Pickaxe p : Pickaxe.values()) {
+            if (cachedLvl == Skills.getRealLevel(Skill.MINING)) {
+                break; // skip loop if you have already found the best pickaxe for this level
+            }
+            if (p.getREQ() > Skills.getRealLevel(Skill.MINING)) {
+                continue;
+            }
+            if (Inventory.contains(p.ID) || Equipment.contains(p.ID) || Bank.contains(p.ID)) {
+                bestOwnedPickaxe = p;
+                cachedLvl = Skills.getRealLevel(Skill.MINING);
+            }
+        }
+
+        if (bestOwnedPickaxe == null) {
+            // todo handle buying
+            MethodProvider.log("we dont own any pickaxe!");
+            return -1;
+        }
+
+        // if you need to get out your pickaxe or deposit items, do that.
+        if ((!Inventory.contains(bestOwnedPickaxe.ID) && Equipment.contains(bestOwnedPickaxe.ID))
+                || Inventory.contains(miningFilter)) {
+            if (Walking.shouldWalk() && Bank.openClosest()) {
+                if (Inventory.contains(miningFilter)) {
+                    Bank.depositAllItems();
+                    return Timing.loopReturn();
+                }
+                Bank.withdraw(bestOwnedPickaxe.ID);
+            }
+        }
+
 
 
         return Timing.loopReturn();
